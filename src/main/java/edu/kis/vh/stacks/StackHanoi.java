cee79ec1edb33165c15227f3063fd266abe5644d
Anamitr <sochacki.konrad@gmail.com>
2017-03-03 11:31:05 +0100

    3.3.2

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,13 +1,27 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Klasa rozszerzajca klase Stack, na ktorym nie mozna polozyc liczby wiekszej niz obecna na wierzchu stosu
+ * @author Konrad Sochacki
+ * @version 3.3.2
+ * @see Stack
+ * 
+ */
 public class StackHanoi extends Stack {
-
+	/**
+	 * Pole reprezentujace ilosc odrzuconych elementow
+	 */
 	private int totalRejected = 0;
-
+	/**
+	 * Funkcja zwracajaca pole totalRejected, w zasadzie getter
+	 * @return totalRejected
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
-	
+	/**
+	 * Nadpisana funkcja, wkladajaca element na stos o ile jest to mozliwe
+	 */
 	@Override
 	public void push(int in) {
 		if (!isEmpty() && in > top())
