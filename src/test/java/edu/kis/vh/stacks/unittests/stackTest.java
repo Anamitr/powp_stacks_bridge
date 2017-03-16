437783eca29de319993aef662a4f8aa464fc78ac
Konrad Sochacki <195059@edu.p.lodz.pl>
2017-03-16 15:28:12 +0100

    3.1.9

--- src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
+++ src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import edu.kis.vh.stacks.Stack;
-import edu.kis.vh.stacks.list.StackArray;
+import edu.kis.vh.stacks.implementations.StackArray;
 
 // 3.2.1 Wszystkie testy przechodza pomyslnie
 
