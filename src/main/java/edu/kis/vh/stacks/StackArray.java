e6d5a6fefc1e42cfbea2c810c6640929a789a8fd
Konrad Sochacki <195059@edu.p.lodz.pl>
2017-03-16 15:20:52 +0100

    3.1.5

--- src/main/java/edu/kis/vh/stacks/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.stacks;
 
-public class StackArray {
+public class StackArray implements IStack {
 
 	private static final int EMPTY_STACK_VALUE = -1;
 	private static final int NUM_OF_ITEMS = 12;
@@ -8,29 +8,35 @@ public class StackArray {
 	private int[] items = new int[NUM_OF_ITEMS];
 	private int total = EMPTY_STACK_VALUE;
 
+	@Override
 	public int getTotal() {
 		return total;
 	}
 
+	@Override
 	public void push(int i) {
 		if (!isFull())
 			items[++total] = i;
 	}
 
+	@Override
 	public boolean isEmpty() {
 		return total == -1;
 	}
 
+	@Override
 	public boolean isFull() {
 		return total == 11;
 	}
 
+	@Override
 	public int top() {
 		if (isEmpty())
 			return -1;
 		return items[total];
 	}
 
+	@Override
 	public int pop() {
 		if (isEmpty())
 			return -1;
